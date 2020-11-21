c1a74b78210f017fd7fa972d12a8d21c265ef254
TamerMograbi <tomispk@gmail.com>
2020-11-21 13:01:13 +0200

    add all 6 numbers and margin between them

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import React,{useState} from "react"
 function App() {
   const [num1,setNum1] = useState("");
   const [num2,setNum2] = useState("");
+  const [num3,setNum3] = useState("");
+  const [num4,setNum4] = useState("");
+  const [num5,setNum5] = useState("");
+  const [num6,setNum6] = useState("");
 
-  const stateFuncs = [setNum1,setNum2];
+  const stateFuncs = [setNum1,setNum2,setNum3,setNum4,setNum5,setNum6];
   //instead of creating 6 handle functions
   //this map takes the set function of a state
   //and returns a function that calls it with a param t
@@ -16,10 +20,15 @@ function App() {
 
   return (
     <div className="App">
-        <h1> hello world</h1>
-        <h3>will start playing with lottery simulator here!</h3>
+        <h1> Lottery Sim</h1>
+      <div className="NumContainer">
         <LotteryNumber text={num1} onNumChange={handleFuncs[0]}/>
         <LotteryNumber text={num2} onNumChange={handleFuncs[1]}/>
+        <LotteryNumber text={num3} onNumChange={handleFuncs[2]}/>
+        <LotteryNumber text={num4} onNumChange={handleFuncs[3]}/>
+        <LotteryNumber text={num5} onNumChange={handleFuncs[4]}/>
+        <LotteryNumber text={num6} onNumChange={handleFuncs[5]}/>
+      </div>
     </div>
   );
 }
