e3b2b8bd0082d91710df3b7fd693833d588b855d
TamerMograbi <tomispk@gmail.com>
2020-11-21 12:44:49 +0200

    add map functon that creates are change handlers

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ function App() {
   const [num1,setNum1] = useState("");
   const [num2,setNum2] = useState("");
 
-  function handleTextChange(newText) {
-    setNum1(newText)
-  }
-  function handleNum2Change(newText) {
-    setNum2(newText)
-  }
+  const stateFuncs = [setNum1,setNum2];
+  //instead of creating 6 handle functions
+  //this map takes the set function of a state
+  //and returns a function that calls it with a param t
+  const handleFuncs = stateFuncs.map((curr_func) => {
+    return (t) => curr_func(t);
+  })
+
   return (
     <div className="App">
         <h1> hello world</h1>
         <h3>will start playing with lottery simulator here!</h3>
-        <LotteryNumber text={num1} onNumChange={handleTextChange}/>
-        <LotteryNumber text={num2} onNumChange={handleNum2Change}/>
+        <LotteryNumber text={num1} onNumChange={handleFuncs[0]}/>
+        <LotteryNumber text={num2} onNumChange={handleFuncs[1]}/>
     </div>
   );
 }
