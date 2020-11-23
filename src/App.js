d085a92330b4c9f61a2664b0f6a12da5d0c0cc95
TamerMograbi <tomispk@gmail.com>
2020-11-23 21:42:03 +0200

    remove semicolons from js files

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-import './css/App.css';
+import './css/App.css'
 import LotteryNumber from './LotteryNumber'
 import React,{useEffect, useState} from "react"
 
 function App() {
-  const MEGA_BALL_INDEX = 5;
-  const MEGA_BALL_MAX = 25;
-  const NORMAL_BALL_MAX = 70;
+  const MEGA_BALL_INDEX = 5
+  const MEGA_BALL_MAX = 25
+  const NORMAL_BALL_MAX = 70
 
-  const [num1,setNum1] = useState("");
-  const [num2,setNum2] = useState("");
-  const [num3,setNum3] = useState("");
-  const [num4,setNum4] = useState("");
-  const [num5,setNum5] = useState("");
-  const [num6,setNum6] = useState("");
-  const [generatednum,setGeneratedNum] = useState([]);
-  const [myCount,setMyCount] = useState(0);
-  const [isSimStarted,setIsSimStarted] = useState(false);
+  const [num1,setNum1] = useState("")
+  const [num2,setNum2] = useState("")
+  const [num3,setNum3] = useState("")
+  const [num4,setNum4] = useState("")
+  const [num5,setNum5] = useState("")
+  const [num6,setNum6] = useState("")
+  const [generatednum,setGeneratedNum] = useState([])
+  const [myCount,setMyCount] = useState(0)
+  const [isSimStarted,setIsSimStarted] = useState(false)
 
-  const stateFuncs = [setNum1,setNum2,setNum3,setNum4,setNum5,setNum6];
-  const nums = [num1,num2,num3,num4,num5,num6];
+  const stateFuncs = [setNum1,setNum2,setNum3,setNum4,setNum5,setNum6]
+  const nums = [num1,num2,num3,num4,num5,num6]
   //instead of creating 6 handle functions
   //this map takes the set function of a state
   //and returns a function that calls it with a param t
   const handleFuncs = stateFuncs.map((curr_func) => {
-    return (t) => curr_func(t);
+    return (t) => curr_func(t)
   })
 
   const mappedLotteryNumbers = nums.map((num,index) => {
-    let isMegaBall = false;
+    let isMegaBall = false
     if(index === MEGA_BALL_INDEX) {
-      isMegaBall = true;
+      isMegaBall = true
     }
     return <LotteryNumber key={index} text={num} onNumChange={handleFuncs[index]} isMegaBall={isMegaBall}/>
   })
 
   useEffect(() => {
     if(isSimStarted) {
-      setMyCount(prevCount => prevCount + 1);
-      const rndNums = getSixRandomNums();
+      setMyCount(prevCount => prevCount + 1)
+      const rndNums = getSixRandomNums()
       const isWinning = rndNums.every((num,index) => num === nums[index])
       if(isWinning) {
-        console.log('unbelievable you won!');
+        console.log('unbelievable you won!')
       }
       setGeneratedNum(rndNums)
     }
   },[myCount])
 
   async function handleClick() {
-    setIsSimStarted(prevVal => !prevVal);
+    setIsSimStarted(prevVal => !prevVal)
     if(!isSimStarted) {
-      setMyCount(prev => prev + 1);
+      setMyCount(prev => prev + 1)
     }
     
   }
@@ -78,7 +78,7 @@ function App() {
       <h2>{generatednum.join(" ")}</h2>
       <h2>Number Of Trials: {myCount}</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
