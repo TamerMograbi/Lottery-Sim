3152f44b769ee13c2ad9cc0696e1177e0adbd075
TamerMograbi <tomispk@gmail.com>
2020-11-24 17:20:33 +0200

    show days years centuries. there is a bug when reaches centuries

--- src/LotteryInfo.js
+++ src/LotteryInfo.js
@@ -1,12 +1,14 @@
 import React from "react"
 import './css/LotteryInfo.css'
+import {getDaysYearsPassed} from './util'
 
 function LotteryInfo(props) {
+    const numOfTrials = getDaysYearsPassed(props.countTrials)
     return (
         <div className="lotteryInfo">
             <h2>{props.genNum}</h2>
             <h2>closest Match: {props.closestMatch} number of Matches: {props.numOfMatches}</h2>
-            <h2>Number Of Trials: {props.countTrials}</h2>
+            <h2>Number Of Trials: {numOfTrials}</h2>
             {props.isGameWon && <h1>You won!! random = {props.genNum}</h1>}
         </div>
     )
--- src/util.js
+++ src/util.js
@@ -1,5 +1,8 @@
 import {MEGA_BALL_MAX,NORMAL_BALL_MAX,MEGA_BALL_INDEX} from './App'
 
+const DAYS_IN_A_YEAR = 365
+const DAYS_IN_A_CENTURY = 36500
+
 function getSixRandomNums() {
     let rndNums = new Array(6).fill(0)
     return rndNums.map((num,index) => {
@@ -16,4 +19,19 @@ function getSixRandomNums() {
 function getMatches(arr1,arr2) {
     return arr1.map((val,index) => val === arr2[index])
 }
-  export {getSixRandomNums,getMatches}
+
+function getDaysYearsPassed(num) {
+  let res = ""
+  if(num < DAYS_IN_A_YEAR) {
+    res = num.toString() + ' days'
+  } else if(num < DAYS_IN_A_CENTURY){
+    res = (Math.round(num/DAYS_IN_A_YEAR)).toString() + ' years ' + (num%DAYS_IN_A_YEAR).toString() + ' days'
+  } else {
+    let centuries = Math.round(num/DAYS_IN_A_CENTURY)
+    let yearsAfterRemovingCenturies = Math.round(num%DAYS_IN_A_CENTURY/DAYS_IN_A_YEAR)
+    res = (centuries).toString() + (centuries === 1 ? ' century ' : ' centuries ')
+     +  (yearsAfterRemovingCenturies).toString() + ' years'
+  }
+  return res
+}
+  export {getSixRandomNums,getMatches,getDaysYearsPassed}
