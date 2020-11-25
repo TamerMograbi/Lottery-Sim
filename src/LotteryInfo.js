776291ceb212cb27a64ec6a8ccce1a1e0c9aec33
TamerMograbi <tomispk@gmail.com>
2020-11-25 19:12:30 +0200

    conditionally render closest match

--- src/LotteryInfo.js
+++ src/LotteryInfo.js
@@ -17,8 +17,8 @@ function LotteryInfo(props) {
     return (
         <div className="lotteryInfo">
             <h2>{props.genNum}</h2>
-            <h2>closest Match: {mappedSpans}</h2>
-            <h2>Number Of Trials: {numOfTrials}</h2>
+            {props.countTrials > 0 && <h2>Closest Match: {mappedSpans}</h2>}
+            <h2>Time Passed: {numOfTrials}</h2>
             {props.isGameWon && <h1>You won!! random = {props.genNum}</h1>}
         </div>
     )
