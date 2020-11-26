13543e1da5d2b121b221273010fee438507ba781
TamerMograbi <tomispk@gmail.com>
2020-11-26 11:59:20 +0200

    thank you flexbox for making it easy to vertically align

--- src/HugeTitleBar.js
+++ src/HugeTitleBar.js
@@ -5,6 +5,7 @@ function HugeTitleBar(props) {
     return (
         <div className={"titleBar"}>
             <h1 className="title"> Lottery Sim</h1>
+            <p>See just how bad your odds are :)</p>
         </div>
     )
 }
