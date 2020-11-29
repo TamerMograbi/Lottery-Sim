541c5e3ae596d610cb64b0a31c2e0e8229da0547
TamerMograbi <tomispk@gmail.com>
2020-11-29 22:24:34 +0200

    add developed with React to footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
     return (
         <div class="footerContainer">
             <p className="footerText">Developed with React</p>
+            <img className="footerImage" src="logo192.png"/>
         </div>
     )
 }
